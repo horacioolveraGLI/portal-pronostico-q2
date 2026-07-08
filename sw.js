9fca64c6dfc66ca5e19d95a8ea1e50a942dceacb
horacioolveraGLI <horacioolvera282@gmail.com>
2026-07-08 00:02:52 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Service Worker básico para Portal Pronóstico Q2
 // Hace la app instalable y guarda el index.html en caché para arranque offline
-const CACHE = 'pronostico-q2-v49';
+const CACHE = 'pronostico-q2-v58';
 const ASSETS = [
   './',
   './index.html',
